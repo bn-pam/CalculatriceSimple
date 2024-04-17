2d4d513fa662a30a492b0e73d0bd2121f6b59750
Fatima EL JATTIOUI <fjattioui@gmail.com>
2024-04-17 16:42:16 +0200

    ajout de la fonction addition

---
+++ addition.py
@@ -0,0 +1,16 @@
+def addition(a, b):
+    """
+    Cette fonction additionne deux chiffres.
+
+    Arguments :
+    a (int, float) : Le premier chiffre.
+    b (int, float) : Le deuxième chiffre.
+
+    Retourne :
+    int, float : La somme des deux chiffres.
+    """
+    return a + b
+
+# Exemple d'utilisation de la fonction
+resultat = addition(5, 3)
+print("Résultat de l'addition :", resultat)
