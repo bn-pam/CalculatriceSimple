bbf80ca8064b6f8e4076838c7c8681dfd3831bd2
bn-pam <bonneau.pam@gmail.com>
2024-04-30 15:25:44 +0200

    merge boucle while pour les fonctions, ajout ci yaml, ajout covering pytest

--- calculatrice/src/main.py
+++ calculatrice/src/main.py
@@ -1,48 +1,81 @@
-from addition import addition
-from soustraction import soustraction
-from multiplication import multiplication
-from squarred import squarred
-from division import division
+from calculatrice.src.addition import addition
+from calculatrice.src.soustraction import soustraction
+from calculatrice.src.multiplication import multiplication
+from calculatrice.src.squarred import squarred
+from calculatrice.src.division import division
 
-choix = ""
-while choix != "q":
-    print("A pour addition, S pour soustraction, M pour multiplication, D pour division, R pour racine_carrée")
-    print("q pour sortir")
-    choix = input("indiquer votre choix : ")
-    if choix == "A" or choix == "a" :
-        nombre1 = int(input("donner le premier nombre : "))
-        nombre2 = int(input("donner un autre nombre : "))
-        result = addition(nombre1, nombre2)
-        print(f"le résultat est {result}")
-        print("")
-        
-    elif choix == "S" or choix == "s" :
-        nombre1 = int(input("donner le premier nombre : "))
-        nombre2 = int(input("donner un autre nombre : "))
-        result = soustraction(nombre1, nombre2)
-        print(f"le résultat est {result}")
-        print("")
 
-    elif choix == "R" or choix == "r" :
-        nombre1 = int(input("donner le nombre: "))
-        result = squarred(nombre1)
-        print(f"le résultat est {result}")
-        print("")
+def calculatrice():
+    choix = ""
+    while choix != "q":
+        print("A pour addition, S pour soustraction, M pour multiplication, D pour division, R pour racine_carrée")
+        print("q pour sortir")
+        choix = input("indiquer votre choix : ")
+        if choix == "A" or choix == "a" :
+            nombre1_str = input("donner le premier nombre : ")
+            while not nombre1_str.isdigit():
+                nombre1_str = input("merci de rentrer un nombre :")
+            nombre1 = int(nombre1_str)
+            nombre2_str = input("donner un autre nombre : ")
+            while not nombre2_str.isdigit() :
+                nombre2_str = input("merci de rentrer un nombre")
+            nombre2 = int(nombre2_str)
+            
+            result = addition(nombre1, nombre2)
+            print(f"le résultat est {result}")
+            print("")
+            
+        elif choix == "S" or choix == "s" :
+            nombre1_str = input("donner le premier nombre : ")
+            while not nombre1_str.isdigit():
+                nombre1_str = input("merci de rentrer un nombre :")
+            nombre1 = int(nombre1_str)
+            nombre2_str = input("donner un autre nombre : ")
+            while not nombre2_str.isdigit() :
+                nombre2_str = input("merci de rentrer un nombre")
+            nombre2 = int(nombre2_str)
+            result = soustraction(nombre1, nombre2)
+            print(f"le résultat est {result}")
+            print("")
 
-    elif choix == "M" or choix == "m" :
-        nombre1 = int(input("donner le premier nombre : "))
-        nombre2 = int(input("donner un autre nombre : "))
-        result = multiplication(nombre1, nombre2)
-        print(f"le résultat est {result}")
-        print("")
-        
-    elif choix == "D" or choix == "d" :
-        nombre1 = int(input("donner le premier nombre : "))
-        nombre2 = int(input("donner un autre nombre : "))
-        result = division(nombre1, nombre2)
-        print(f"le résultat est {result}")
-        print("")
-        
-    else :
-        print("le choix n'est pas correct")
-        print("")
+        elif choix == "R" or choix == "r" :
+            nombre1_str = input("donner le premier nombre : ")
+            while not nombre1_str.isdigit():
+                nombre1_str = input("merci de rentrer un nombre :")
+            nombre1 = int(nombre1_str)
+            result = squarred(nombre1)
+            print(f"le résultat est {result}")
+            print("")
+
+        elif choix == "M" or choix == "m" :
+            nombre1_str = input("donner le premier nombre : ")
+            while not nombre1_str.isdigit():
+                nombre1_str = input("merci de rentrer un nombre :")
+            nombre1 = int(nombre1_str)
+            nombre2_str = input("donner un autre nombre : ")
+            while not nombre2_str.isdigit() :
+                nombre2_str = input("merci de rentrer un nombre")
+            nombre2 = int(nombre2_str)
+            result = multiplication(nombre1, nombre2)
+            print(f"le résultat est {result}")
+            print("")
+            
+        elif choix == "D" or choix == "d" :
+            nombre1_str = input("donner le premier nombre : ")
+            while not nombre1_str.isdigit():
+                nombre1_str = input("merci de rentrer un nombre :")
+            nombre1 = int(nombre1_str)
+            nombre2_str = input("donner un autre nombre : ")
+            while not nombre2_str.isdigit() :
+                nombre2_str = input("merci de rentrer un nombre")
+            nombre2 = int(nombre2_str)
+            result = division(nombre1, nombre2)
+            print(f"le résultat est {result}")
+            print("")
+            
+        else :
+            print("le choix n'est pas correct")
+            print("")
+
+if __name__ == "__main__":
+    calculatrice()
